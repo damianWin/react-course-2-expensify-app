6629c44822e75f701d3dfca43b7b37794845bf9d
damian win <damianwin9demarzo@gmail.com>
2021-10-22 11:59:59 -0300

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -15,26 +15,9 @@ import selectExpenses from "./selectors/expensesSelector"
 
 const store = configureStore(); // combineReducers
 
-store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Gas bill", createdAt: 10000000 }));
-store.dispatch(addExpense({ description: "rent", amount: 100000, createdAt: 10006 }))
-// store.dispatch(removeExpense({ descriptione: "mobile bill", amount: 1000}))
-// store.dispatch(setTextFilter("Vi testar va")) ????
 
-
-const state = store.getState();
-const visibleExpenses = selectExpenses(state.expenses, state.filters); 
-// selectExpenses(expenses, { text, sortBy, startDate, endDate })
-console.log(`state.expenses: `)
-console.log( state.expenses) 
-
-console.log(`state.filters: `)
-console.log(state.filters)
-
-console.log("visibleExpenses: ", visibleExpenses);
-
-
-console.log("store.getState() at app.js: ", store.getState());
+// const state = store.getState();
+// const visibleExpenses = selectExpenses(state.expenses, state.filters); 
 
 const jsx = (
     // The prop `store` is required in `Provider`
